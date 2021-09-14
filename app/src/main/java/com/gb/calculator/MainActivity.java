39571718ebbd74d00b0cd999bcf487d640791bd0
Pasha <alkatrazzzzz@mail.ru>
2021-09-14 21:11:44 +0300

    Initial commit

---
+++ app/src/main/java/com/gb/calculator/MainActivity.java
@@ -0,0 +1,33 @@
+package com.gb.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+Button zeroButton;
+Button oneButton;
+Button twoButton;
+Button threeButton;
+Button fourButton;
+Button fiveButton;
+Button sixButton;
+Button sevenButton;
+Button eightButton;
+Button nineButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.users_viev);
+
+
+
+
+    }
+}
